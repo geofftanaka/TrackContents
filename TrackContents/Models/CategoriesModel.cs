bf517301f39847abfc0d6d6d8c32fff73561ad20
Geoff Tanaka <geoff.tanaka@gmail.com>
2023-03-03 14:35:13 -0700

    feat: added resource file
    - Added resource file to save connection string

--- TrackContents/Models/CategoriesModel.cs
+++ TrackContents/Models/CategoriesModel.cs
@@ -1,16 +1,19 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
+using System.Reflection;
+using System.Resources;
 
 namespace TrackContents.Models
 {
 
     public class CategoriesModel
     {
-        const string connectionString = "Data Source=HEPHAESTUS\\MSSQLSERVER01;Initial Catalog=TrackContents;Integrated Security=SSPI;";
+        string connectionString = String.Empty;
 
         public CategoriesModel()
         {
-
+            ResourceManager rm = new ResourceManager("TrackContents.Resources.Data", Assembly.GetExecutingAssembly());
+            connectionString = rm.GetString("ConnectionString");
         }
                
         public async Task<List<Category>> GetCategories()
